243f936009eac7da943f124f990dfa4371445415
Andre Antunes Vieira <andreantunesv@gmail.com>
2020-10-03 16:29:40 -0300

    style

--- php/nivel-1/desafio-1/index.php
+++ php/nivel-1/desafio-1/index.php
@@ -2,15 +2,48 @@
 
 require('./Resposta.php');
 
-
+function validate($name){
+    if($name === 'resposta1'){
+        global $resposta1;
+        return json_encode($resposta1) === '[1,2,3,4,5,6,7,8,9,10]';
+    }
+}
+function validateClass($name){
+    return validate($name) ? 'right' : 'wrong';
+}
+function resposta($name){
+    global $resposta1;
+    $respostas = ['resposta1' => $resposta1];
+    return json_encode($respostas[$name]);
+}
+function expect($name){
+    if($name === 'resposta1') return '[1,2,3,4,5,6,7,8,9,10]';
+    return '[]';
+}
 ?>
 <!DOCTYPE html>
 <html lang="pt">
 <head>
     <meta charset="UTF-8">
     <title>Resposta</title>
+    <link rel="stylesheet" href="https://raw.githubusercontent.com/DevsDeNegocio/ensinando-loops/master/common/style.css"/>
 </head>
 <body>
-Exemplo teste
+<section>
+    <h2>$resposta1</h2>
+    <article class="<?=validateClass('resposta1')?>">
+        <label>
+            $resposta1 deve ser um array de 1 a 10
+        </label>
+        <div class="description">
+            Resposta esperada:
+        </div>
+        <b><?=expect('resposta1')?></b>
+        <div class="description">
+            Resposta encontrada:
+        </div>
+        <b><?=resposta('resposta1')?></b>
+    </article>
+</section>
 </body>
 </html>
